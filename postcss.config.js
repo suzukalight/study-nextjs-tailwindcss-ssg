1b85d7cbfce5e8b117c29334c41ca5d601f02cdf
suzukalight <mail@mkubara.com>
2020-04-25 23:22:47 +0900

    Setup purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,19 @@
+const purgecss = [
+  "@fullhuman/postcss-purgecss",
+  {
+    content: [
+      "./components/**/*.{js,jsx,ts,tsx}",
+      "./pages/**/*.{js,jsx,ts,tsx}",
+    ],
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+  },
+];
+
 module.exports = {
-  plugins: ["tailwindcss", "postcss-preset-env"],
+  plugins: [
+    "tailwindcss",
+    "autoprefixer",
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+    "postcss-preset-env",
+  ],
 };
